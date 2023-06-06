0c60c836d6439fafc5a7e797f7c9de7aa05d1b7b
pooreumu <pooreum83@gmail.com>
2023-06-06 13:33:15 +0900

    🧑‍💻 eslint unused import plugin 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin','unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
   ],
   root: true,
   env: {
